7a0d3eb48ffb7dc1f2eec8c38286b53d5b436662
kingborehaha <55667610+kingborehaha@users.noreply.github.com>
2022-08-05 15:33:14 -0700

    Handle cross-map mass selection in object list

--- StudioCore/MsbEditor/SceneTree.cs
+++ StudioCore/MsbEditor/SceneTree.cs
@@ -352,19 +352,26 @@ namespace StudioCore.MsbEditor
                 {
                     // Select Range
                     var entList = e.Container.Objects;
-                    var i1 = entList.IndexOf((MapEntity)_selection.GetSelection().First());
+                    var i1 = entList.IndexOf((MapEntity)_selection.GetSelection().FirstOrDefault(fe => ((MapEntity)fe).Container == e.Container));
                     var i2 = entList.IndexOf((MapEntity)e);
 
-                    var iStart = i1;
-                    var iEnd = i2;
-                    if (i2 < i1)
+                    if (i1 != -1 && i2 != -1)
                     {
-                        iStart = i2;
-                        iEnd = i1;
+                        var iStart = i1;
+                        var iEnd = i2;
+                        if (i2 < i1)
+                        {
+                            iStart = i2;
+                            iEnd = i1;
+                        }
+                        for (var i = iStart; i <= iEnd; i++)
+                        {
+                            _selection.AddSelection(entList[i]);
+                        }
                     }
-                    for (var i = iStart; i <= iEnd; i++)
+                    else
                     {
-                        _selection.AddSelection(entList[i]);
+                        _selection.AddSelection(e);
                     }
                 }
                 else
