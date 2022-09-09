256ada1bfcf554545976fda29ab5b8335bb1bc54
Philiquaz <philiquaz@gmail.com>
2022-09-09 10:02:56 +0100

    Clearly separated crashable tasks from noncrashables. (#119)
    * Clearly separated crashable tasks from noncrashables.
    
    * Remove unwanted file

--- StudioCore/ParamEditor/ParamBank.cs
+++ StudioCore/ParamEditor/ParamBank.cs
@@ -649,7 +649,7 @@ namespace StudioCore.ParamEditor
             
             CacheBank.ClearCaches();
 
-            TaskManager.Run("PB:LoadParams", true, false, true, () =>
+            TaskManager.Run("PB:LoadParams", true, false, false, () =>
             {
                 if (AssetLocator.Type != GameType.Undefined)
                 {
@@ -728,7 +728,7 @@ namespace StudioCore.ParamEditor
                         }
                         IsLoadingVParams = false;
 
-                        TaskManager.Run("PB:RefreshDirtyCache", true, false, false, () => refreshParamDirtyCache());
+                        TaskManager.Run("PB:RefreshDirtyCache", true, true, true, () => refreshParamDirtyCache());
                     });
                 }
 
