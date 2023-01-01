86cf6d3d4a7ee49d491589ea03f7784b12b16cc4
Philiquaz <Philiquaz@gmail.com>
2023-01-01 23:41:45 +0000

    Add average argument

--- StudioCore/ParamEditor/MassParamEdit.cs
+++ StudioCore/ParamEditor/MassParamEdit.cs
@@ -657,6 +657,16 @@ namespace StudioCore.ParamEditor
                     return (c) => v;
                 };
             }));
+            argumentGetters.Add("average", (2, (field) => (param) => {
+                PseudoColumn pc = field[0].Equals("ID") ? PseudoColumn.ID : field[0].Equals("Name") ? PseudoColumn.Name : PseudoColumn.None;
+                Param.Column? col = param?[field[0]];
+                if (pc == PseudoColumn.None && col == null)
+                    throw new Exception($@"Could not locate field {field[0]}");
+                var rows = RowSearchEngine.rse.Search((ParamBank.PrimaryBank, param), field[1], false, false);
+                var vals = rows.Select((row, i) => pc == PseudoColumn.ID ? row.ID : pc == PseudoColumn.Name ? row.Name : row[col].Value);
+                double avg = vals.Average((val) => Convert.ToDouble(val));
+                return (row) => (c) => avg.ToString();
+            }));
             argumentGetters.Add("vanilla", (0, (empty) => {
                 ParamBank bank = ParamBank.VanillaBank;
                 return (param) => {
