b3406ed25234474fb6512f01ba46569321bfcc8a
Philiquaz <Philiquaz@gmail.com>
2023-01-20 09:23:34 +0000

    add random argumentGetters

--- StudioCore/ParamEditor/MassParamEdit.cs
+++ StudioCore/ParamEditor/MassParamEdit.cs
@@ -740,6 +740,31 @@ namespace StudioCore.ParamEditor
                     };
                 };
             }));
+            argumentGetters.Add("random", (2, (minAndMax) => {
+                double min;
+                double max;
+                if (!double.TryParse(minAndMax[0], out min) || !double.TryParse(minAndMax[1], out max))
+                    throw new Exception($@"Could not parse min and max random values");
+                if (max <= min)
+                    throw new Exception($@"Random max must be greater than min");
+                double range = max - min;
+                return (param) => (row) => (c) => (Random.Shared.NextDouble() * range + min).ToString();
+            }));
+            argumentGetters.Add("randint", (2, (minAndMax) => {
+                int min;
+                int max;
+                if (!int.TryParse(minAndMax[0], out min) || !int.TryParse(minAndMax[1], out max))
+                    throw new Exception($@"Could not parse min and max randint values");
+                if (max <= min)
+                    throw new Exception($@"Random max must be greater than min");
+                return (param) => (row) => (c) => Random.Shared.NextInt64(min, max + 1).ToString();
+            }));
+            argumentGetters.Add("randFrom", (3, (paramRowsAndField) => {
+                Param srcParam = ParamBank.PrimaryBank.Params[paramRowsAndField[0]];
+                List<Param.Row> srcRows = RowSearchEngine.rse.Search((ParamBank.PrimaryBank, srcParam), paramRowsAndField[1], false, false);
+                object[] values = srcRows.Select((r, i) => r[paramRowsAndField[2]].Value.Value).ToArray();
+                return (param) => (row) => (c) => values[Random.Shared.NextInt64(values.Length)].ToString();
+            }));
         }
 
         internal Func<Param, Func<Param.Row, Func<(PseudoColumn, Param.Column), string>>>[] getContextualArguments(int argumentCount, string opData)
