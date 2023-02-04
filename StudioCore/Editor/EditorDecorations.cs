dbb8af3ed950ab29a49aba8dc1c6a91f7393c980
Philiquaz <philiquaz@gmail.com>
2023-02-04 21:38:40 +0000

    Fix context menus when using only some decorations (#340)

--- StudioCore/Editor/EditorDecorations.cs
+++ StudioCore/Editor/EditorDecorations.cs
@@ -239,7 +239,7 @@ namespace StudioCore.Editor
         
         public static bool ParamRefEnumContextMenu(ParamBank bank, object oldval, ref object newval, List<ParamRef> RefTypes, Param.Row context, FMGBank.FMGInfo fmgInfo, ParamEnum Enum)
         {
-            if (RefTypes == null && Enum == null && fmgInfo == null)
+            if ((CFG.Current.Param_HideReferenceRows || RefTypes == null) && (CFG.Current.Param_HideEnums || Enum == null) && (CFG.Current.Param_HideReferenceRows || fmgInfo == null))
                 return false;
             if (ImGui.IsItemClicked(ImGuiMouseButton.Left) && (InputTracker.GetKey(Veldrid.Key.ControlLeft) || InputTracker.GetKey(Veldrid.Key.ControlRight)))
             {
