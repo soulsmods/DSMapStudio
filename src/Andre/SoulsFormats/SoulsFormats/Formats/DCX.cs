d5e05adb197faa1f794552777dc95b27636e9bdf
Shadowth117 <shadic@msn.com>
2024-03-17 23:30:41 -0400

    Add 0x100000 bytes to be extra sure we have enough buffer (#795)
    * Add 0x100000 bytes to be extra sure we have enough buffer
    
    * Move to MemoryStream(capacity) and shrink results if expanding occurred.
    
    ---------
    
    Co-authored-by: Philiquaz <Philiquaz@gmail.com>

--- src/Andre/SoulsFormats/SoulsFormats/Formats/DCX.cs
+++ src/Andre/SoulsFormats/SoulsFormats/Formats/DCX.cs
@@ -286,8 +286,7 @@ namespace SoulsFormats
             if (egdtSize != 0x24 + chunkCount * 0x10)
                 throw new InvalidDataException("Unexpected EgdT size in EDGE DCX.");
 
-            byte[] decompressed = new byte[uncompressedSize];
-            using (MemoryStream dcmpStream = new MemoryStream(decompressed))
+            using (MemoryStream dcmpStream = new MemoryStream(uncompressedSize))
             {
                 for (int i = 0; i < chunkCount; i++)
                 {
@@ -309,9 +308,8 @@ namespace SoulsFormats
                         dcmpStream.Write(chunk, 0, chunk.Length);
                     }
                 }
+                return dcmpStream.GetBytes();
             }
-
-            return decompressed;
         }
 
         private static byte[] DecompressDCXDFLT(BinaryReaderEx br, Type type)
