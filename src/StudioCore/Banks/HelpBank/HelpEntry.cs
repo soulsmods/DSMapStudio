0122b8bd06bd385f5ab7889770d4498a3818a671
Vawser <ben.rouncefield@gmail.com>
2024-06-04 23:11:34 +0100

    Help Overhaul (#896)
    * feat: New Help JSON
    
    * feat: Help Window
    
    - Overhauled the Help Window (Smithbox implementation)
    - Removed Smithbox help json
    
    * Moved to core dev team
    
    ---------
    
    Co-authored-by: Philiquaz <philiquaz@gmail.com>

---
+++ src/StudioCore/Banks/HelpBank/HelpEntry.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using System.Text.Json.Serialization;
+
+namespace StudioCore.Help;
+
+[JsonSourceGenerationOptions(WriteIndented = true,
+    GenerationMode = JsonSourceGenerationMode.Metadata, IncludeFields = true)]
+[JsonSerializable(typeof(HelpEntry))]
+internal partial class HelpEntrySerializerContext : JsonSerializerContext
+{
+}
+
+public class HelpEntry
+{
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public int ProjectType { get; set; }
+    public List<string> Tags { get; set; }
+    public List<string> Contents { get; set; }
+    public List<float> HeaderColor { get; set; }
+    public List<float> HighlightColor { get; set; }
+}
